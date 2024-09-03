311731b6d2140fe57510ff26881fd0e265457a0f
avacado-green <159917869+avacado-green@users.noreply.github.com>
2024-09-03 21:12:09 +0300

    Update controls.py

--- controls.py
+++ controls.py
@@ -8,10 +8,13 @@ def events(gun, screen, bullets):
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
+            """Привсваивание клавиши d к правому повороту"""
             if event.key == pygame.K_d:
                 gun.mright = True
+            """Присваивание клавиши a к левому повороту"""
             elif event.key == pygame.K_a:
                 gun.mleft = True
+            """Присваичание клавиши пробел(SPACE) к стрельбе"""
             elif event.key == pygame.K_SPACE:
                 new_bullet = Bullet(screen, gun)
                 bullets.add(new_bullet)
